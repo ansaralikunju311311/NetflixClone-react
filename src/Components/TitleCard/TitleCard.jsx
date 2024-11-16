2dc3ab2915611ffd87bb961990a9ec0606d2c839
ansaralikunju123 <ansarpanoor311@gmail.com>
2024-11-16 19:47:34 +0530

    dummyhome

--- src/Components/TitleCard/TitleCard.jsx
+++ src/Components/TitleCard/TitleCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import cards_data from '../../assets/cards/Cards_data.js'
 import './TitleCard.css'
-const TitleCard = () => {
+const TitleCard = ({title,category}) => {
   return (
     <div className='titlecards'>
-      
+      <h2>{title?title:"Popolular on Netlix"}</h2>
+       <div className="card-list">
+          {cards_data.map((card,index)=>{
+              return <div className="card" key={index}>
+                <img src={card.image} alt="" />
+                 <p>{card.name}</p>
+              </div>
+        })}
+       </div>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,45 @@
 import React from 'react'
 import './Home.css'
 import NavBar from '../../Components/NavBar/NavBar'
+import hero_banner from '../../assets/hero_banner.jpg';
+import hero_title from '../../assets/hero_title.png';
+import  play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCard from '../../Components/TitleCard/TitleCard';
+import Footer from '../../Components/Footer/Footer';
 const Home = () => {
   return (
     <div className='home'>
-    <NavBar />
+       <NavBar />
+    <div className='hero'>
+            <img src={hero_banner} alt=""  className='banner-img' />
+      
+    <div className="hero-caption">
+      <img src={hero_title} alt=""  className='caption-img'/>
+      <p>Discovering his ties to a secret ancident order , a young man living in modern istabul embarks on a quest to save the city from an immoral enemy</p>
+      <div className="hero-btns">
+        <button className='btn'><img src={play_icon} alt="" />play</button>
+        <button className='btn dark-btn'><img src={info_icon} alt="" />More info</button>
+      </div>
+      <TitleCard />
+
+      </div>
+      
+      </div>
+      <div className="more-cards">
+
+      <TitleCard  title={"Block Buster Movie"}/>
+      <TitleCard  title={"Only on Netflix"}/>
+      <TitleCard  title={"Upcoming"}/>
+      <TitleCard  title={"Topic for You"}/>
+      </div>
+
+
+      
+      <Footer />
     </div>
+    
+  
   )
 }
 
