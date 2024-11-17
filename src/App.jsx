bf7f7be12f60083012c7b60d6d2b7de48503f726
ansaralikunju123 <ansarpanoor311@gmail.com>
2024-11-17 11:37:34 +0530

    setloginandsign

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
 const App = () => {
   return (
     <div>
-      <Home />
+    <Routes>
+      <Route path='/' element={   <Home />}/>
+      <Route path='/login' element={<Login />}/>
+    </Routes>
+   
     </div>
   )
 }
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
+
 const Login = () => {
+  const [signState,setState] = useState('Sign In')
   return (
-    <div>
+    <div className='login'>
+      <img src={logo} alt=""  className='login-logo'/>
       
+      <form action="" className='login-form'>
+      <h1>{signState}</h1>
+      {signState==='Sign Up'? <input type="text" placeholder='your name'/>:<></>}
+       
+        <input type="email"  placeholder='email'/>
+        <input type="password"  placeholder='password'/>
+        <button>{signState}</button>
+        <div className='form-help'>
+          <div className="remeber">
+            <input type="checkbox" />
+            <label htmlFor="">Remeber Me</label>
+          </div>
+          <p>Need Help?</p>
+        </div>
+        <div className="form-switch">
+        {signState==="Sign In"?
+         <p>New to Netflix <span onClick={()=>setState("Sign Up")}>Sign Up Now</span></p>
+         : <p>Already have an account? <span onClick={()=>setState("Sign In")}>Sign in Now</span></p>}
+      </div>
+      </form>
+     
     </div>
   )
 }
